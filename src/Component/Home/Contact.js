b5089742f9d6a6842e451831e98b304091e721ef
akhandsingh4700 <77077439+akhandsingh4700@users.noreply.github.com>
2021-04-11 20:45:32 +0530

    Add files via upload

---
+++ src/Component/Home/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import TopBar from "./TopBar";
+
+
+const contacts = () => {
+    return ( <div>
+  <TopBar />
+
+  This Is Contact Page
+  
+
+
+    </div> );
+}
+ 
+export default contacts;
